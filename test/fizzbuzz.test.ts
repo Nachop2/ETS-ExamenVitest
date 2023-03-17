57fca479795252410db94f448b698b6b49cf2994
Nachop2 <MY_NAME@example.com>
2023-03-17 18:52:57 +0000

    Fizzbuzz testing update

--- test/fizzbuzz.test.ts
+++ test/fizzbuzz.test.ts
@@ -15,6 +15,9 @@ describe("fizzbuzz",(): void => {
     it("Le damos el valor 3, el cual deberia igualar a Revilla", (): void => {
         expect(fizzbuzz(3)).equals("Revilla")
     })
+    it("Le damos el valor 6, el cual deberia igualar a IgnacioRevilla", (): void => {
+        expect(fizzbuzz(6)).equals("IgnacioRevilla")
+    })
     
     it("Le damos el valor 11, el cual deberia igualar a Italiyankina", (): void => {
         expect(fizzbuzz(11)).equals("Italiyankina")
